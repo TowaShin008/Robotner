380092a859287b4ecd22998873700318c187c484
LE3B_ATSUSHI_HIRAZAKURA <k019g1327@g.neec.ac.jp>
2022-05-27 15:02:35 +0900

    ロボットの自動運転（直進）・ロボットの移動速度調整

--- Script/Robot/RobotController.cs
+++ Script/Robot/RobotController.cs
@@ -2,22 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RobotController : MonoBehaviour
 {
     private Rigidbody rigidbody;
-    private float speed = 0.02f;
+    private float speed = 0.08f;
+    private bool stop = true;
+    private Vector3 vec = Vector3.zero;
     TransmitterCounter transmitterCounter;
+    [SerializeField] private GameObject textObject;
+    private Text textComponent;
+
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         rigidbody = GetComponent<Rigidbody>();
         transmitterCounter = GameObject.Find("TransmitterCounter").GetComponent<TransmitterCounter>();
+        textComponent = textObject.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(stop == true)
+        {
+            vec = Vector3.zero;
+        }
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -28,7 +39,8 @@ public class RobotController : MonoBehaviour
             MoveBack();
         }
 
-
+        transform.position += vec;
+         
         if (Input.GetKey(KeyCode.A))
         {
             RotateLeft();
@@ -38,9 +50,13 @@ public class RobotController : MonoBehaviour
             RotateRight();
         }
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StopOnOff();
+        }
 
-        //”­M‹@Ý’u
-        if (Input.GetKeyDown(KeyCode.Z))
+            //”­M‹@Ý’u
+            if (Input.GetKeyDown(KeyCode.Z))
         {
             //rigidbody.AddForce(new Vector3(0, 0, 5));
             if(transmitterCounter.remaining >= 1) {
@@ -52,18 +68,27 @@ public class RobotController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
+        if (collision.gameObject.tag != "Wall") { return; }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            transform.position -= vec;
+        }
+
+        stop = true;
+        textComponent.text = "Ž©“®‘€c:OFF";
     }
 
     public void MoveForward()
     {
-        transform.position += transform.forward * speed;
+        vec = transform.forward * speed;
     }
 
     public void MoveBack()
     {
-        transform.position -= transform.forward * speed;
+        vec = -(transform.forward * speed);
     }
 
     public void RotateRight()
@@ -74,4 +99,17 @@ public class RobotController : MonoBehaviour
     {
         transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime, Space.World);
     }
+
+    public void StopOnOff()
+    {
+        if(stop == true) { 
+            stop = false;
+            textComponent.text = "Ž©“®‘€c:ON";
+        }
+        else { 
+            stop = true;
+            textComponent.text = "Ž©“®‘€c:OFF";
+        }
+    }
+
 }
