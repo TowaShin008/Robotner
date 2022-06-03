d79b6dbe06db1837bf61278ba4d6d8cf63b2c8fc
LE3B_ATSUSHI_HIRAZAKURA <k019g1327@g.neec.ac.jp>
2022-06-03 15:46:35 +0900

    ロボットのライトのスプライトの修正（Unity内で実行中でも数値を変更できるように修正）

--- Script/Robot/RobotLight.cs
+++ Script/Robot/RobotLight.cs
@@ -6,7 +6,6 @@ public class RobotLight : MonoBehaviour
 {
 
     [SerializeField] GameObject robotLight;
-    private Light robLight;
     public float range = 5.0f;
     public float spotAngle = 80.0f;
     public float intensity = 15.0f;
@@ -14,16 +13,20 @@ public class RobotLight : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        robLight = robotLight.AddComponent<Light>();
-        robLight.type = LightType.Spot;
-        robLight.range = range;
-        robLight.spotAngle = spotAngle;       
-        robLight.intensity = intensity;
+        robotLight.GetComponent<Light>().type = LightType.Spot;
+        robotLight.GetComponent<Light>().range = range;
+        robotLight.GetComponent<Light>().spotAngle = spotAngle;
+        robotLight.GetComponent<Light>().intensity = intensity;
     }
 
     // Update is called once per frame
     void Update()
     {
+        robotLight.GetComponent<Light>().type = LightType.Spot;
+        robotLight.GetComponent<Light>().range = range;
+        robotLight.GetComponent<Light>().spotAngle = spotAngle;
+        robotLight.GetComponent<Light>().intensity = intensity;
+
         Vector3 pos = robot.transform.position;
         robotLight.transform.position = pos;
         Quaternion rot = robot.transform.rotation;
