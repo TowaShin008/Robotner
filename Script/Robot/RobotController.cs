5f785f129782a26acca4febbb71a0b33cd681ed2
LE3B_ATSUSHI_HIRAZAKURA <k019g1327@g.neec.ac.jp>
2022-06-03 12:46:12 +0900

    ロボット自動運転（ぶつかったら向きを変えるところまで）

--- Script/Robot/RobotController.cs
+++ Script/Robot/RobotController.cs
@@ -7,12 +7,20 @@ using UnityEngine.UI;
 public class RobotController : MonoBehaviour
 {
     private Rigidbody rigidbody;
-    private float speed = 0.08f;
-    private bool stop = true;
+    private float speed = 0.02f;
+    private bool modeAuto = false;
+    private bool modeTurn = false;
+    private bool right = true;
+    private bool isBack = false;
+    private bool isTurn = false;
+    private int turnCount = 0;
+    private int backCount = 0;
     private Vector3 vec = Vector3.zero;
     TransmitterCounter transmitterCounter;
     [SerializeField] private GameObject textObject;
+    [SerializeField] private GameObject textObject2;
     private Text textComponent;
+    private Text textComponent2;
 
     // Start is called before the first frame update
     void Start()
@@ -20,15 +28,44 @@ public class RobotController : MonoBehaviour
         rigidbody = GetComponent<Rigidbody>();
         transmitterCounter = GameObject.Find("TransmitterCounter").GetComponent<TransmitterCounter>();
         textComponent = textObject.GetComponent<Text>();
+        textComponent2 = textObject2.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(stop == true)
+        if (modeAuto == false && isBack == true)
+        {
+            backCount++;
+
+            if (backCount <= 30)
+            {
+                transform.position -= vec * 2;
+            }
+            else
+            {
+                isBack = false;
+            }            
+        }
+        else if (modeAuto == false && isTurn == true)
+        {
+            turnCount++;
+
+            if (turnCount < 360)
+            {
+                RotateRight();
+            }
+            else
+            {
+                isTurn = false;
+            }
+        }
+        else if (modeAuto == false)
         {
             vec = Vector3.zero;
         }
+       
+
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -38,9 +75,12 @@ public class RobotController : MonoBehaviour
         {
             MoveBack();
         }
+        if (isBack == false && isTurn == false)
+        {
+            transform.position += vec;
+        }
 
-        transform.position += vec;
-         
+
         if (Input.GetKey(KeyCode.A))
         {
             RotateLeft();
@@ -52,11 +92,16 @@ public class RobotController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            StopOnOff();
+            ModeAutoOnOff();
         }
 
-            //”­M‹@Ý’u
-            if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            ModeTurnOnOff();
+        }
+
+        //”­M‹@Ý’u
+        if (Input.GetKeyDown(KeyCode.Z))
         {
             //rigidbody.AddForce(new Vector3(0, 0, 5));
             if(transmitterCounter.remaining >= 1) {
@@ -72,13 +117,28 @@ public class RobotController : MonoBehaviour
     {
         if (collision.gameObject.tag != "Wall") { return; }
 
-        for (int i = 0; i <= 5; i++)
-        {
-            transform.position -= vec;
-        }
+        //for (int i = 0; i <= 10; i++)
+        //{
+        //    transform.position -= vec;
+        //}
 
-        stop = true;
-        textComponent.text = "Ž©“®‘€c:OFF";
+        if(modeAuto == true)
+        {
+            isBack = true;
+            backCount = 0;
+            modeAuto = false;
+            textComponent.text = "Ž©“®‘€c:OFF";
+
+            if (modeTurn == true)
+            {
+                RotateRight();
+                modeTurn = false;
+                isTurn = true;
+                turnCount = 0;                
+                textComponent2.text = "‰ñ“]:OFF";
+            }
+        }
+        vec = Vector3.zero;
     }
 
     public void MoveForward()
@@ -100,16 +160,28 @@ public class RobotController : MonoBehaviour
         transform.Rotate(new Vector3(0, -90, 0) * Time.deltaTime, Space.World);
     }
 
-    public void StopOnOff()
+    public void ModeAutoOnOff()
     {
-        if(stop == true) { 
-            stop = false;
+        if(modeAuto == false) { 
+            modeAuto = true;
             textComponent.text = "Ž©“®‘€c:ON";
         }
         else { 
-            stop = true;
+            modeAuto = false;
             textComponent.text = "Ž©“®‘€c:OFF";
         }
     }
-
+    public void ModeTurnOnOff()
+    {
+        if (modeTurn == true)
+        {
+            modeTurn = false;
+            textComponent2.text = "‰ñ“]:OFF";
+        }
+        else
+        {
+            modeTurn = true;
+            textComponent2.text = "‰ñ“]:ON";
+        }
+    }
 }
